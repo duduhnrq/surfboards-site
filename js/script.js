437bd4366b7ece56be4540dee148e482e920ef03
Eduardo Henrique <dudu30rafa@gmail.com>
2025-03-23 17:07:12 -0300

    implementei uma animação de texto utilizando js

--- js/script.js
+++ js/script.js
@@ -42,3 +42,13 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li",
 );
 mobileNavbar.init();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const heroContent = document.querySelector('.hero-content');
+    const heroImage = document.querySelector('.hero-image');
+    setTimeout(() => {
+        heroContent.classList.add('visible');
+        heroImage.classList.add('visible');
+    }, 100);
+});
+
